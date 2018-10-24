85d3180449fc297e7a5ea08a39b021c63a2d14e2
Rommel García Hernández <rommelgh@gmail.com>
2018-10-24 14:29:56 -0500

    Update memory_allocation3.c

--- memory_allocation3.c
+++ memory_allocation3.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double *multiplyByTwo (double *input) {
+double *multiply_by_two (double *input) {
   double *twice = malloc(sizeof(double));
   *twice = *input * 2.0;
   return twice;
 }
 
-int main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
   int *age = malloc(sizeof(int));
   *age = 30;
   double *salary = malloc(sizeof(double));
   *salary = 12345.67;
-  double *myList = malloc(3 * sizeof(double));
-  myList[0] = 1.2;
-  myList[1] = 2.3;
-  myList[2] = 3.4;
+  double *my_list = malloc(3 * sizeof(double));
+  my_list[0] = 1.2;
+  my_list[1] = 2.3;
+  my_list[2] = 3.4;
 
-  double *twiceSalary = multiplyByTwo(salary);
-
-  printf("double your salary is %.3f\n", *twiceSalary);
+  double *twice_salary = multiply_by_two(salary);
+  printf("double your salary is %.3f\n", *twice_salary);
 
   free(age);
   free(salary);
-  free(myList);
-  free(twiceSalary);
+  free(my_list);
+  free(twice_salary);
 
   return 0;
 }
